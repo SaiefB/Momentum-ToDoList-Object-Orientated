650d5a4427b1b1566ce4503d6e0b2efbd74c654c
Saief <A.S.Bokor@outlook.com>
2025-04-10 22:35:22 +0100

    Add task class constructor to logic file

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,22 @@
 //logic.js
 
+// Task Section
+// Task Class Constructor
+class Task {
+    constructor(taskTitle, taskDescription, taskDueDate, taskPriority, taskProject) {
+        console.log("-----Task Constructor Initiated")
+        this.taskTitle = taskTitle;
+        this.taskDescription = taskDescription;
+        this.taskDueDate = taskDueDate;
+        this.taskPriority = taskPriority;
+        this.taskProject = taskProject;
+    }
+}
+
+// Form Validation - checks if the form is filled out
+
 // Project Section
+//Project Class Constructor
 class Project {
     constructor(projectTitle) {
         this.projectTitle = projectTitle;
