4c448400467473e225d74a2131b3e31c09199d1f
Saief <A.S.Bokor@outlook.com>
2025-04-17 15:44:09 +0100

    index.js initialised
    refactored index.js ready for new code

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@
 
 import "./styles.css";
 
+import {} from "./modules/DOM";
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+    // Initialize the application
+    // You can call your initialization functions here
+    // For example:
+    // initializeApp();
+});
